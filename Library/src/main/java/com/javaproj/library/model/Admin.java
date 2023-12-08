7e433c08818f1fcc11b3ef8c8bc0ca0531a92d9a
OstFl <workostfl@gmail.com>
2023-12-08 19:21:08 +0100

    initalized the project and created main/develop/OstapFlorchuk branches

---
+++ Library/src/main/java/com/javaproj/library/model/Admin.java
@@ -0,0 +1,11 @@
+package com.javaproj.library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+public class Admin {
+
+}
