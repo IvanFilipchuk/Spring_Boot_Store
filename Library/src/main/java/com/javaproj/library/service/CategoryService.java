f9bd01cb684e63a2169cd661a679ec9172b43e22
DEZOL <ckwjakdz@gmail.com>
2023-12-09 19:42:01 +0100

    Adding category and product to Admin module

---
+++ Library/src/main/java/com/javaproj/library/service/CategoryService.java
@@ -0,0 +1,25 @@
+package com.javaproj.library.service;
+
+import com.javaproj.library.dto.CategoryDto;
+import com.javaproj.library.model.Category;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryService {
+    Category save(Category category);
+
+    Category update(Category category);
+
+    List<Category> findAllByActivatedTrue();
+
+    List<Category> findALl();
+
+    Optional<Category> findById(Long id);
+
+    void deleteById(Long id);
+
+    void enableById(Long id);
+
+    List<CategoryDto> getCategoriesAndSize();
+}
