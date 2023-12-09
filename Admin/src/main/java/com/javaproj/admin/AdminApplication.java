cc82c10976bbff752f16437361dc191d7f52cbd3
OstFl <workostfl@gmail.com>
2023-12-09 16:27:46 +0100

    added login/register/forgotpassw functionality added and edite d bootstrap templates, added authcontroller

--- Admin/src/main/java/com/javaproj/admin/AdminApplication.java
+++ Admin/src/main/java/com/javaproj/admin/AdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.javaproj.library.*"})
+@SpringBootApplication(scanBasePackages = {"com.javaproj.library.*","com.javaproj.admin.*"})
 @EnableJpaRepositories(value = "com.javaproj.library.repository")
 @EntityScan(value = "com.javaproj.library.model")
 public class AdminApplication {
--- Library/src/main/java/com/javaproj/library/dto/AdminDto.java
+++ Library/src/main/java/com/javaproj/library/dto/AdminDto.java
@@ -9,12 +9,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AdminDto {
-    @Size(min = 3, max = 10, message = "First name contains 3-10 characters")
+    @Size(min = 3, max = 10, message = "First name must contain 3-10 characters")
     private String firstName;
-    @Size(min = 3, max = 10, message = "Last name contains 3-10 characters")
+    @Size(min = 3, max = 10, message = "Last name must contain 3-10 characters")
     private String lastName;
     private String username;
-    @Size(min = 5, max = 10, message = "Password contains 5-10 characters")
+    @Size(min = 5, max = 10, message = "Password must contain 5-10 characters")
     private String password;
     private String repeatPassword;
 }
